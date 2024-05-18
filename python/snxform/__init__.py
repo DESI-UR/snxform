966ff9264f4674c09e5774615ce61687b1d99850
Segev BenZvi <segev.benzvi@rochester.edu>
2024-05-17 21:16:17 -0400

    Add module init.

---
+++ python/snxform/__init__.py
@@ -0,0 +1,13 @@
+# Licensed under a 3-clause BSD style license - see LICENSE.rst
+# -*- coding: utf-8 -*-
+"""
+=======
+snxform
+=======
+Supernova identification in spectroscopic data.
+"""
+
+import os
+
+src_path = os.path.realpath(__path__[0])
+base_path = os.sep.join(src_path.split(os.sep)[:-2])
